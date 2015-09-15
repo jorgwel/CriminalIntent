c4e502f6c5a3da48da43b0e1f254d099131cacd5
Jorge Bautista <jorge.bautista@globant.com>
2015-09-15 11:09:59 -0500

    Now the crime data described in the list is being updated on clicking back button

--- app/src/main/java/geoquiz/book/criminalintent/CrimeListFragment.java
+++ app/src/main/java/geoquiz/book/criminalintent/CrimeListFragment.java
@@ -26,6 +26,12 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,8 +50,12 @@ public class CrimeListFragment extends Fragment {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes =  crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
 
     }
 
