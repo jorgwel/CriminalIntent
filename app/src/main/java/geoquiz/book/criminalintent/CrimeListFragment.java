4d257445799d464856d2bc554c40271d12905811
Jorge Bautista <jorge.bautista@globant.com>
2015-09-23 09:47:06 -0500

    Now the |+New Crime| button is being shown

--- app/src/main/java/geoquiz/book/criminalintent/CrimeListFragment.java
+++ app/src/main/java/geoquiz/book/criminalintent/CrimeListFragment.java
@@ -6,7 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -23,8 +27,37 @@ import geoquiz.book.criminalintent.model.CrimeLab;
  */
 public class CrimeListFragment extends Fragment {
 
+    private static final String TAG = "CrimeListFragment";
+
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item_new_crime:
+                Log.d(TAG, "Botón +Crime");
+                return true;
+
+            default:
+                Log.d(TAG, "Ninguna acción manejada por el menú");
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+    }
 
     @Override
     public void onResume() {
