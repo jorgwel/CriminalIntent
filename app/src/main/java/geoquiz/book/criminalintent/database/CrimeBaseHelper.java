460ec3f9b5d3780f6dc6354d995d932c7994bac7
Jorge Bautista <jorge.bautista@globant.com>
2015-09-24 16:57:11 -0500

    Added CrimeBaseHelper.java and CrimeDbSchema.java

---
+++ app/src/main/java/geoquiz/book/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,42 @@
+package geoquiz.book.criminalintent.database;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import geoquiz.book.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by jorge.bautista on 24/09/15.
+ */
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "CrimeBaseHelper";
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "On method CrimeBaseHelper.onCreate");
+        StringBuilder qb = new StringBuilder(150);
+        qb.append("create table ").append(CrimeTable.NAME).append("(")
+          .append("_id integer primary key autoincrement, ")
+          .append(CrimeTable.Columns.TITLE).append(", ")
+          .append(CrimeTable.Columns.DATE).append(", ")
+          .append(CrimeTable.Columns.SOLVED).append(") ");
+
+        db.execSQL(qb.toString());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(TAG, "On method CrimeBaseHelper.onUpgrade");
+    }
+}
---
+++ app/src/main/java/geoquiz/book/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,19 @@
+package geoquiz.book.criminalintent.database;
+
+/**
+ * Created by jorge.bautista on 24/09/15.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+
+        public static final String NAME = "Crimes";
+
+        public static final class Columns {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+
+    }
+}
